1b40e9385e67e1303a1a6d18c3d18231abb23b07
Archsx <871728578@qq.com>
2019-04-18 19:03:09 +0800

    vue

---
+++ vue/slot-scope/slot-scope-c.vue
@@ -0,0 +1,40 @@
+<template>
+    <div >
+        <div v-for="contact in contacts" :key="contact.id">
+            <img :src="contact.picture.medium" alt="">
+            <div>
+                <div>
+                    {{methodFromP(contact)}}
+                </div>
+                <div>
+                    <slot :contact="contact"></slot>
+                </div>
+                <div>
+                    {{contact.email}}
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['methodFromP'],
+    data(){
+        return {
+            contacts:[]
+        }
+    },
+    created() {
+        fetch('/contacts.json')
+            .then(response => response.json())
+            .then(contacts => this.contacts = contacts)  
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/slot-scope/slot-scope-p.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div>
+            <!-- 直接给子组件传一个函数，用这个函数去自定义子组件的渲染方式，这样其实功能很弱，只是为了凸显scope-slot的强大 -->
+            <contact-list :methodFromP = "(dataInChild)=>dataInChild.name.first"></contact-list>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import ContactList from './component/ContactList.vue'
+
+export default {
+    components:{
+        ContactList,
+    },
+    data(){
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
